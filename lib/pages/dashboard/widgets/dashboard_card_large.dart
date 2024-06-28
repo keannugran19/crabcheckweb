15abcac8d4ab0ace2b189579fef79d7733df07da
keannugran19 <main.grankeannu@gmail.com>
2024-06-28 22:27:04 +0800

    ADD pie chart for crab counts

--- lib/pages/dashboard/widgets/dashboard_card_large.dart
+++ lib/pages/dashboard/widgets/dashboard_card_large.dart
@@ -3,8 +3,8 @@ import 'package:crabcheckweb1/constants/colors.dart';
 import 'package:crabcheckweb1/pages/dashboard/barGraph/bar_graph.dart';
 import 'package:crabcheckweb1/pages/dashboard/barGraph/bar_graph_lists.dart';
 import 'package:crabcheckweb1/pages/dashboard/widgets/info_card.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class DashboardPageLargeScreen extends StatefulWidget {
   const DashboardPageLargeScreen({super.key});
@@ -125,8 +125,6 @@ class _DashboardPageLargeScreenState extends State<DashboardPageLargeScreen> {
 
     return Column(
       children: [
-        // first row displayed
-
         // Page restart button
         Align(
           alignment: Alignment.topRight,
@@ -137,9 +135,12 @@ class _DashboardPageLargeScreenState extends State<DashboardPageLargeScreen> {
                 color: colorScheme.primary,
               )),
         ),
+
         const SizedBox(
           height: 10,
         ),
+
+        // first row displayed
         Row(
           children: [
             InfoCard(
@@ -233,7 +234,6 @@ class _DashboardPageLargeScreenState extends State<DashboardPageLargeScreen> {
           ],
         ),
 
-        // BAR GRAPH
         Padding(
           padding: const EdgeInsets.fromLTRB(0, 20.0, 0.0, 2.0),
           child: Text(
@@ -245,23 +245,60 @@ class _DashboardPageLargeScreenState extends State<DashboardPageLargeScreen> {
             ),
           ),
         ),
-        Container(
-            height: 300,
-            decoration: BoxDecoration(
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                      offset: const Offset(0, 6),
-                      color: Colors.grey.withOpacity(.1),
-                      blurRadius: 12),
-                ],
-                borderRadius: BorderRadius.circular(8)),
-            child: Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: BarGraph(
-                totalCrabs: graphData,
-              ),
-            ))
+
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            // BAR GRAPH
+            Expanded(
+              flex: 2,
+              child: Container(
+                  height: 300,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                            offset: const Offset(0, 6),
+                            color: Colors.grey.withOpacity(.1),
+                            blurRadius: 12),
+                      ],
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: BarGraph(
+                      totalCrabs: graphData,
+                    ),
+                  )),
+            ),
+
+            // PIE CHART
+            Expanded(
+                flex: 1,
+                child: SizedBox(
+                    height: 300,
+                    child: PieChart(
+                        swapAnimationDuration:
+                            const Duration(milliseconds: 750),
+                        swapAnimationCurve: Curves.easeInOutQuint,
+                        PieChartData(sections: [
+                          PieChartSectionData(
+                              value: scyllaSerrataCount.toDouble(),
+                              color: Colors.brown),
+                          PieChartSectionData(
+                              value: scyllaOlivaceaCount.toDouble(),
+                              color: Colors.orange),
+                          PieChartSectionData(
+                              value: scyllaParamamosainCount.toDouble(),
+                              color: Colors.green),
+                          PieChartSectionData(
+                              value: portunosPelagicusCount.toDouble(),
+                              color: Colors.grey),
+                          PieChartSectionData(
+                              value: zosimusAeneusCount.toDouble(),
+                              color: Colors.purple),
+                        ]))))
+          ],
+        ),
       ],
     );
   }
