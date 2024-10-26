40348c5bebeb5d7447522f71dd6b68c73ba7e986
keannugran19 <main.grankeannu@gmail.com>
2024-10-26 11:07:52 +0800

    ADD nominatim api to convert latlong to address

--- lib/pages/report/widgets/reports_table.dart
+++ lib/pages/report/widgets/reports_table.dart
@@ -1,8 +1,11 @@
+import 'dart:convert';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:crabcheckweb1/services/firestore.dart';
-import 'package:data_table_2/data_table_2.dart';
 import 'package:flutter/material.dart';
+import 'package:data_table_2/data_table_2.dart';
 import 'package:intl/intl.dart';
+import 'package:crabcheckweb1/services/firestore.dart';
+import 'package:http/http.dart' as http;
 
 class ReportsTable extends StatefulWidget {
   const ReportsTable({super.key});
@@ -12,116 +15,237 @@ class ReportsTable extends StatefulWidget {
 }
 
 class _ReportsTableState extends State<ReportsTable> {
-  final FirestoreService firestoreService = FirestoreService();
-  String? selectedYear; // Holds the selected year
-  final List<String> years = [
-    '2024',
-    '2025',
-    '2026',
-    '2027',
-    '2028'
-  ]; // Available years for sorting
+  static const _rowsPerPage = 5;
+  static const List<String> _years = ['2024', '2025', '2026', '2027', '2028'];
+  static final _dateFormat = DateFormat('MMMM d, yyyy HH:mm');
 
-  final int _rowsPerPage = 5; // Number of rows per page for pagination
-  int _currentPage = 0; // Current page index
+  final FirestoreService _firestoreService = FirestoreService();
+  String? _selectedYear;
+  int _currentPage = 0;
+
+  void _onYearChanged(String? value) {
+    setState(() {
+      _selectedYear = value;
+      _currentPage = 0;
+    });
+  }
+
+  Stream<QuerySnapshot<Map<String, dynamic>>> _getFilteredStream() {
+    if (_selectedYear == null) {
+      return _firestoreService.crabs
+          .orderBy('timestamp', descending: true)
+          .snapshots();
+    }
+
+    final int year = int.parse(_selectedYear!);
+    final start = DateTime(year);
+    final end = DateTime(year + 1);
+
+    return _firestoreService.crabs
+        .where('timestamp', isGreaterThanOrEqualTo: start)
+        .where('timestamp', isLessThan: end)
+        .orderBy('timestamp', descending: true)
+        .snapshots();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        // Dropdown Button for selecting year
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            DropdownButton<String>(
-              value: selectedYear,
-              hint: const Text("Select Year"),
-              items: years.map((year) {
-                return DropdownMenuItem<String>(
-                  value: year,
-                  child: Text(year),
-                );
-              }).toList(),
-              onChanged: (value) {
-                setState(() {
-                  selectedYear = value;
-                  _currentPage = 0; // Reset to first page when year is changed
-                });
-              },
-            ),
-          ],
-        ),
+        _buildYearFilter(),
         SizedBox(
-          height: (56 * 5) + 40,
-          child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-            stream: _getSortedStream(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(child: CircularProgressIndicator());
-              } else if (snapshot.hasError) {
-                return Center(child: Text('Error: ${snapshot.error}'));
-              } else if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-                return const Center(child: Text('No data available.'));
-              }
-
-              final rows = _createRows(snapshot.data!);
-              final paginatedRows = _paginateRows(rows);
-
-              return Column(
-                children: [
-                  Expanded(
-                    child: DataTable2(
-                      columnSpacing: 20,
-                      horizontalMargin: 12,
-                      headingRowColor: WidgetStateProperty.all(
-                        Colors.grey.withOpacity(0.1),
-                      ),
-                      dividerThickness: 0.5,
-                      dataRowColor: WidgetStateProperty.all(Colors.white),
-                      columns: [
-                        DataColumn2(
-                          size: ColumnSize.S,
-                          label: Text("Image", style: _cellTextStyle),
-                        ),
-                        DataColumn2(
-                          size: ColumnSize.M,
-                          label: Text("Species", style: _cellTextStyle),
-                        ),
-                        DataColumn2(
-                          size: ColumnSize.S,
-                          label: Text('Edibility', style: _cellTextStyle),
-                        ),
-                        DataColumn2(
-                          size: ColumnSize.L,
-                          label: Text('Location (Latitude & Longitude)',
-                              style: _cellTextStyle),
-                        ),
-                        DataColumn2(
-                          size: ColumnSize.L,
-                          label: Text('Date & Time', style: _cellTextStyle),
-                        ),
-                        const DataColumn2(
-                          size: ColumnSize.S,
-                          label: Text(''),
-                        ),
-                      ],
-                      rows: paginatedRows,
-                    ),
-                  ),
-                  _buildPaginationControls(rows.length),
-                ],
-              );
-            },
-          ),
+          height: (56 * _rowsPerPage) + 40,
+          child: _buildTable(),
         ),
       ],
     );
   }
 
-  // Pagination Controls
+  Widget _buildYearFilter() {
+    return Align(
+      alignment: Alignment.centerRight,
+      child: DropdownButton<String>(
+        value: _selectedYear,
+        hint: const Text("Select Year"),
+        items: _years.map((year) {
+          return DropdownMenuItem<String>(
+            value: year,
+            child: Text(year),
+          );
+        }).toList(),
+        onChanged: _onYearChanged,
+      ),
+    );
+  }
+
+  Widget _buildTable() {
+    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+      stream: _getFilteredStream(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        }
+
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return const Center(child: Text('No data available.'));
+        }
+
+        final allRows = _createDataRows(snapshot.data!);
+        final paginatedRows = _paginateRows(allRows);
+
+        return Column(
+          children: [
+            Expanded(child: _buildDataTable(paginatedRows)),
+            _buildPaginationControls(allRows.length),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildDataTable(List<DataRow> rows) {
+    return DataTable2(
+      columnSpacing: 20,
+      horizontalMargin: 12,
+      headingRowColor: WidgetStateProperty.all(Colors.grey.withOpacity(0.1)),
+      dividerThickness: 0.5,
+      dataRowColor: WidgetStateProperty.all(Colors.white),
+      columns: _buildColumns(),
+      rows: rows,
+    );
+  }
+
+  List<DataColumn2> _buildColumns() {
+    const textStyle = TextStyle(color: Colors.black87, fontSize: 14);
+
+    return const [
+      DataColumn2(size: ColumnSize.S, label: Text("Image", style: textStyle)),
+      DataColumn2(size: ColumnSize.M, label: Text("Species", style: textStyle)),
+      DataColumn2(
+          size: ColumnSize.S, label: Text('Edibility', style: textStyle)),
+      DataColumn2(
+        size: ColumnSize.L,
+        label: Text('Location', style: textStyle),
+      ),
+      DataColumn2(
+          size: ColumnSize.L, label: Text('Date & Time', style: textStyle)),
+      DataColumn2(size: ColumnSize.S, label: Text('')),
+    ];
+  }
+
+  List<DataRow> _createDataRows(QuerySnapshot<Map<String, dynamic>> snapshot) {
+    return snapshot.docs.map((doc) {
+      final data = doc.data();
+      final location = data['location'] as GeoPoint?;
+      final timestamp = data['timestamp'] as Timestamp?;
+
+      return DataRow(cells: [
+        DataCell(_buildImageCell(data['image'])),
+        DataCell(Text(data['species']?.toString() ?? 'Unknown')),
+        DataCell(Text(data['edibility']?.toString() ?? 'Unknown')),
+        DataCell(
+          FutureBuilder(
+            future: _getAddressFromCoordinates(location),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Text('Loading...');
+              }
+              return Text(snapshot.data.toString());
+            },
+          ),
+        ),
+        DataCell(Text(_formatTimestamp(timestamp))),
+        DataCell(_buildDeleteButton(doc.id)),
+      ]);
+    }).toList();
+  }
+
+  Widget _buildImageCell(String? imageUrl) {
+    return imageUrl != null
+        ? Image.network(imageUrl, width: 60, height: 60)
+        : const Text('No Image');
+  }
+
+  Future<String> _getAddressFromCoordinates(GeoPoint? location) async {
+    if (location == null) return 'Unknown Location';
+
+    final url = Uri.parse(
+      'https://nominatim.openstreetmap.org/reverse?lat=${location.latitude}&lon=${location.longitude}&format=json&addressdetails=1',
+    );
+
+    try {
+      final response = await http.get(url);
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        final address = data['address'];
+        final barangay = address['suburb'] ?? address['village'] ?? '';
+        final city =
+            address['city'] ?? address['town'] ?? address['municipality'] ?? '';
+
+        return 'Brgy. ${barangay.isNotEmpty ? barangay + ', ' : ''}$city City';
+      } else {
+        return 'Error: ${response.statusCode}';
+      }
+    } catch (e) {
+      print('Error retrieving address: $e');
+      return 'Unknown Location';
+    }
+  }
+
+  String _formatTimestamp(Timestamp? timestamp) {
+    return timestamp != null
+        ? _dateFormat.format(timestamp.toDate())
+        : 'Unknown';
+  }
+
+  Widget _buildDeleteButton(String docId) {
+    return Center(
+      child: IconButton(
+        icon: const Icon(Icons.delete),
+        tooltip: 'Delete',
+        onPressed: () => _showDeleteConfirmation(docId),
+      ),
+    );
+  }
+
+  Future<void> _showDeleteConfirmation(String docId) async {
+    final bool? confirmDelete = await showDialog<bool>(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        title: const Text('Confirm Deletion'),
+        content: const Text('Are you sure you want to delete this data?'),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(false),
+            child: const Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(true),
+            child: const Text('Delete'),
+          ),
+        ],
+      ),
+    );
+
+    if (confirmDelete == true) {
+      await _firestoreService.crabs.doc(docId).delete();
+    }
+  }
+
+  List<DataRow> _paginateRows(List<DataRow> rows) {
+    final start = _currentPage * _rowsPerPage;
+    final end = (start + _rowsPerPage).clamp(0, rows.length);
+    return rows.sublist(start, end);
+  }
+
   Widget _buildPaginationControls(int totalRowCount) {
     final totalPages = (totalRowCount / _rowsPerPage).ceil();
+
     return Container(
       decoration: BoxDecoration(
         color: Colors.grey.withOpacity(0.1),
@@ -143,22 +267,14 @@ class _ReportsTableState extends State<ReportsTable> {
                 icon: const Icon(Icons.chevron_left),
                 color: Colors.blueGrey,
                 onPressed: _currentPage > 0
-                    ? () {
-                        setState(() {
-                          _currentPage--;
-                        });
-                      }
+                    ? () => setState(() => _currentPage--)
                     : null,
               ),
               IconButton(
                 icon: const Icon(Icons.chevron_right),
                 color: Colors.blueGrey,
                 onPressed: (_currentPage + 1) < totalPages
-                    ? () {
-                        setState(() {
-                          _currentPage++;
-                        });
-                      }
+                    ? () => setState(() => _currentPage++)
                     : null,
               ),
             ],
@@ -167,98 +283,4 @@ class _ReportsTableState extends State<ReportsTable> {
       ),
     );
   }
-
-  // Paginate rows based on the current page and rows per page
-  List<DataRow> _paginateRows(List<DataRow> rows) {
-    final start = _currentPage * _rowsPerPage;
-    final end = start + _rowsPerPage;
-    return rows.sublist(start, end > rows.length ? rows.length : end);
-  }
-
-  // Stream sorted by the selected year
-  Stream<QuerySnapshot<Map<String, dynamic>>> _getSortedStream() {
-    if (selectedYear != null) {
-      DateTime start = DateTime(int.parse(selectedYear!), 1, 1);
-      DateTime end = DateTime(int.parse(selectedYear!) + 1, 1, 1);
-
-      return firestoreService.crabs
-          .where('timestamp', isGreaterThanOrEqualTo: start)
-          .where('timestamp', isLessThan: end)
-          .orderBy('timestamp', descending: true)
-          .snapshots();
-    } else {
-      return firestoreService.crabs
-          .orderBy('timestamp', descending: true)
-          .snapshots();
-    }
-  }
-
-  // Create rows from snapshot
-  List<DataRow> _createRows(QuerySnapshot<Map<String, dynamic>> snapshot) {
-    return snapshot.docs.map((doc) {
-      final data = doc.data();
-      final image = data['image']?.toString() ?? "No Image";
-      final species = data['species']?.toString() ?? 'Unknown';
-      final edibility = data['edibility']?.toString() ?? 'Unknown';
-      final location = data['location'] as GeoPoint?;
-      final timestamp = data['timestamp'] as Timestamp?;
-
-      final locationText = location != null
-          ? 'Lat: ${location.latitude.toStringAsFixed(6)}, '
-              'Lng: ${location.longitude.toStringAsFixed(6)}'
-          : 'Unknown';
-      final dateTimeText = timestamp != null
-          ? DateFormat('MMMM d, yyyy HH:mm').format(timestamp.toDate())
-          : 'Unknown';
-
-      return DataRow(cells: [
-        DataCell(Image.network(image, width: 60, height: 60)),
-        DataCell(Text(species, style: _cellTextStyle)),
-        DataCell(Text(edibility, style: _cellTextStyle)),
-        DataCell(Text(locationText, style: _cellTextStyle)),
-        DataCell(Text(dateTimeText, style: _cellTextStyle)),
-        DataCell(Center(
-          child: IconButton(
-            onPressed: () async {
-              bool confirmDelete = await showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    title: const Text('Confirm Deletion'),
-                    content: const Text(
-                        'Are you sure you want to delete this data?'),
-                    actions: [
-                      TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop(false);
-                        },
-                        child: const Text('Cancel'),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop(true);
-                        },
-                        child: const Text('Delete'),
-                      ),
-                    ],
-                  );
-                },
-              );
-
-              if (confirmDelete == true) {
-                await firestoreService.crabs.doc(doc.id).delete();
-              }
-            },
-            icon: const Icon(Icons.delete),
-            tooltip: 'Delete',
-          ),
-        )),
-      ]);
-    }).toList();
-  }
-
-  TextStyle get _cellTextStyle => const TextStyle(
-        color: Colors.black87,
-        fontSize: 14,
-      );
 }
